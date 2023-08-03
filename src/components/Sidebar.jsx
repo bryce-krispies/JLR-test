b382c44ea4778eb75d2b32785ac7e200fbfa653d
Bryce Cayanan <brycepcayanan@gmail.com>
2023-08-03 16:24:07 +0100

    Clean up Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,66 +1,50 @@
 import { React, useState, useEffect } from "react";
 import './Sidebar.css';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LogoutIcon from '@mui/icons-material/Logout';
 
 function Sidebar() {
     //Is the sidebar open
-    const [open, setopen] = useState(true);
+    const [open, setOpen] = useState(true);
 
     //Width of the sidebar
-    const [sideBarWidth, setSideBarWidth] = useState('max-content');
+    const [sidebarWidth, setSidebarWidth] = useState('max-content');
 
     //Opens and closes the sidebar
-    const toggleOpen = () => {
-        setopen(!open)
-    }
+    const toggleOpen = () => setOpen(!open);
 
     //Turns the arrow
     const turnStyle = {
         transform: open ? 'rotate(-180deg)' : '',
         transition: 'transform 0.5s ease'
-    }
+    };
 
-    //Changes the sidebar width when opened/closed
-    const sidebarOpen = {
-
-        width: open ? sideBarWidth: document.getElementById("tektelicLogoAdmin").offsetWidth,
+    const sidebarWidthStyling = {
+        width: open ? sidebarWidth : document.getElementById("jlrLogo").offsetWidth,
         overflow: "hidden",
-        transition: 'width 0.5s ease',
-        
-    }
+        transition: 'width 0s ease',
+        padding: open ? "0 0.75rem 0 0.75rem" : "0 0 0 0.75rem"
+    };
 
     //Set the original sidebar width
     useEffect(() => { 
-        if(sideBarWidth === 'max-content')
-            setSideBarWidth(document.getElementById("navBarAdmin").offsetWidth)
+        if(sidebarWidth === 'max-content') { setSidebarWidth(document.getElementById("sidebar").offsetWidth); }
     });
-
-    const [user, setData] = useState("");
 
     //The main html
     return (
-        <div id="navBarAdmin" className="navigation-admin">
-            <div style={sidebarOpen}>
+        <div id="sidebar" className="navigation-admin">
+            <div style={sidebarWidthStyling}>
                 <div className="expanded-header-container">
-                    <img style={{paddingRight:'0.2rem'}} id="tektelicLogoAdmin" src={ require("../assets/tektelic-logo.png") }></img>
-                    <div className="left header-one">Clarity</div>
+                    <img id="jlrLogo" src={ require("../assets/jlr-new-logo.jpg") }></img>
+                    <div className="left header-one">Filter</div>
                 </div>
+                {open && <h1>Jaguar Land Rover</h1>}
             </div>
             <div className="button-container">
                 <button className="navigation-button" onClick={toggleOpen}>
                     <ArrowForwardIosRoundedIcon style={turnStyle}/>
                 </button>
             </div>          
-            <div className="account-container" style = {sidebarOpen}>
-                <div className="same-line" id="bottom-width-admin">
-                    {/* TODO: fix link */}
-                    <AccountCircleIcon id="user-icon" sx={{ fontSize: 40}} />
-                    <span className="bottom-text">{user.firstname} {user.lastname}<br/><span className="role-text">{user.role}</span></span>
-                    <LogoutIcon className="logout-button"></LogoutIcon>
-                </div>
-            </div>
         </div>
     )
 }
