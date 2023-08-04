2947da7faf0a15a5154dd488dfa8415112e27682
Bryce Cayanan <brycepcayanan@gmail.com>
2023-08-04 15:01:53 +0100

    Add Test Datetime and Cell accordions

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,34 @@
-import { React, useState, useEffect } from "react";
+import React from "react";
 import './Sidebar.css';
+import Accordion from "react-bootstrap/Accordion";
+import Form from 'react-bootstrap/Form';
 
 function Sidebar() {
     return (
         <div className="sidebar-container">
-            Hi
+            <h2 style={{margin: "0 auto 0 auto", padding: "0 0 0.5rem 0"}}>Filter</h2>
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                <Accordion.Header><h5 style={{margin: "0"}}>Test Datetime</h5></Accordion.Header>
+                    <Accordion.Body>
+                        <p style={{margin: "0", padding: "0 0 0.5rem 0"}}>Start</p>
+                        <input type="datetime-local"/>
+                        <p style={{margin: "0", padding: "1rem 0 0.5rem 0"}}>End</p>
+                        <input type="datetime-local"/>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                <Accordion.Header><h5 style={{margin: "0"}}>Cell</h5></Accordion.Header>
+                    <Accordion.Body>
+                        <Form>
+                            <Form.Check type={'checkbox'} id={`default-checkbox`} label={`C1`}/>
+                            <Form.Check type={'checkbox'} id={`default-checkbox`} label={`C2`}/>
+                            <Form.Check type={'checkbox'} id={`default-checkbox`} label={`C3`}/>
+                            <Form.Check type={'checkbox'} id={`default-checkbox`} label={`C4`}/>
+                        </Form>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </div>
     );
 }
