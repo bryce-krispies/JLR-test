c291e5846414f27d00e29db56fe203436e2756e7
Bryce Cayanan <brycepcayanan@gmail.com>
2023-08-05 12:46:53 +0100

    Implement functionality (not button) for duplicate toggle

--- src/components/Visualization.jsx
+++ src/components/Visualization.jsx
@@ -2,6 +2,8 @@ import './Visualization.css';
 import Table from 'react-bootstrap/Table';
 
 function Visualization(props) {
+    const removeDuplicates = false;
+    
     return (
         <div className="vis-container">
             <div className="table-container table-responsive">
@@ -21,23 +23,37 @@ function Visualization(props) {
                     </thead>
                     <tbody>
                         {
-                            props.records.filter((entry) => {
-                                if (props.checkedCells.includes(entry['Cell'])) {
-                                    return true;
-                                }
-                                return false;
-                            }).map((entry) => {
-                                var temp = [];
-                                Object.entries(entry).forEach((pair) => {
-                                    if (props.checkedColumns.includes(pair[0])) {
-                                        temp.push(pair[1]);
+                            removeDuplicates ? 
+                                Array.from(new Set(props.records.filter((entry) => {
+                                    if (props.checkedCells.includes(entry['Cell'])) {
+                                        return true;
                                     }
-                                });
-                                return <tr>{temp.map((value) => <td>{value}</td>)}</tr>;
-                            }).filter((entry) => {
-                                /*TODO Filter out duplicates here*/
-                                return true;
-                            })
+                                    return false;
+                                }).map((entry) => {
+                                    var temp = [];
+                                    Object.entries(entry).forEach((pair) => {
+                                        if (props.checkedColumns.includes(pair[0])) {
+                                            temp.push(pair[1]);
+                                        }
+                                    });
+                                    return temp;
+                                }).map(JSON.stringify)), JSON.parse).map((entry) => {
+                                    return <tr>{entry.map((value) => <td>{value}</td>)}</tr>;
+                                }) :
+                                props.records.filter((entry) => {
+                                    if (props.checkedCells.includes(entry['Cell'])) {
+                                        return true;
+                                    }
+                                    return false;
+                                }).map((entry) => {
+                                    var temp = [];
+                                    Object.entries(entry).forEach((pair) => {
+                                        if (props.checkedColumns.includes(pair[0])) {
+                                            temp.push(pair[1]);
+                                        }
+                                    });
+                                    return <tr>{temp.map((value) => <td>{value}</td>)}</tr>;
+                                })
                         }
                     </tbody>
                 </Table>
